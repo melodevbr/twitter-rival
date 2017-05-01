f6cf6e48323abc48738982e2e159be8536f4be45
Hotaviano Melo <hotaviano@melo.biz>
2017-05-01 18:31:08 -0300

    fix bad patterns

--- models/message.rb
+++ models/message.rb
@@ -4,4 +4,9 @@ class Message < ActiveRecord::Base
 
   validates :text, length: { maximum: 140 }
   validates_presence_of :text, :user
+
+  scope :timeline_from, lambda { |user|
+    where(user_id: user.id).or(Message.where(user: user.following))
+                           .order(created_at: :desc)
+  }
 end
